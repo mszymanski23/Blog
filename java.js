be837486b6f3fd38e8dd0fb5dce1f510f3a568a7
Michał Szymański <54045955+mszymanski23@users.noreply.github.com>
2020-03-22 22:40:10 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+/*alert ("Uwaga! ten blok jest tak wciagajacy, że ciężko Ci się będzie od niego oderwać;)");*/
+
+
+
+
+
+/*var tipcalculator = {
+  billValue: [124, 48, 67, 260, 180],
+  calcTip: function() {
+    for (var i = 0; i < 5; i++) {
+      console.log(this.billValue[i]);
+    }
+  }
+}
+
+      /*
+      if (this.billValue < 50) {
+        var tipPercentage = 0.2;
+      } else if (this.billValue  >= 50 && i <= 200) {
+        var tipPercentage = 0.15;
+      } else {
+        var tipPercentage = 0.1;
+      }
+      console.log(billValue[i] * tipPercentage);
+    }
+}
+}
+*/
